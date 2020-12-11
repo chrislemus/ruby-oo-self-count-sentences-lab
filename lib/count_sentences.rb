3e5cce255d85a879f2eacb2aa7de5d5c1624774c
chrislemus <icrislemus@gmail.com>
2020-12-10 21:37:04 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")  
   end
 
   def question?
-
+    self.end_with?("?")  
   end
 
   def exclamation?
-
+    self.end_with?("!") 
   end
 
   def count_sentences
-
+    counter = 0
+    track = ["!", "?", "."]
+    counter += 1 if track.include?(self[-1])
+    self.each_char.with_index {|char, i| 
+      counter += 1 if track.include?(char) && self[i+1] == " " 
+    }
+    counter
   end
 end
